ba54d1444c8fd9aa7129204419d0a0e5c7b9368d
Xiaoping Li <narci2008@gmail.com>
2018-08-30 10:36:57 -0700

    Add create processing bar function

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Processing-Bar
 ## This is a tech interview question
-### Making a Processing-Bar, and follow the requirements below:
+### Making a Processing-Bar, and follow the steps below:
 1. Make a processing-bar 400px width and 40% completed;
 2. Make an animation that fill the bar from 0% to 100% in 3 seconds;
 3. Create 3 processing bars
--- autoFill.js
+++ autoFill.js
@@ -1,7 +1,16 @@
-let completed = 0;
 const barElement = document.getElementsByClassName('processing_bar')[0];
-let index = setInterval(function() {
-  completed++;
-  barElement.style.width = completed.toString() + '%';
-  if (completed === 100) clearInterval(index);
-}, 30);
+const barContainer = document.getElementsByClassName('container')[0];
+
+const createProcessingBar = () => {
+  let completed = 0;
+  let index = setInterval(function() {
+    completed++;
+    barElement.style.width = completed.toString() + '%';
+    if (completed === 100) clearInterval(index);
+  }, 30);
+};
+
+const clickCreate = () => {
+  barContainer.style.display = "block";
+  createProcessingBar();
+};
