8ea2169097b6ff0301c8bafec31b6ad650c5362f
Xiaoping Li <narci2008@gmail.com>
2018-08-30 11:00:13 -0700

    Give id parameter to createBar function

--- autoFill.js
+++ autoFill.js
@@ -1,11 +1,12 @@
 const barElement = document.getElementById('processing_bar_1');
 const barContainer = document.getElementById('container');
 
-const createBar = () => {
+const createBar = (id) => {
   const barDiv = document.createElement("div");
   barDiv.style.width = "0%";
   barDiv.style.height = "50px";
   barDiv.style.color = "#42e2f4";
+  barDiv.id = "processing_bar_" + id.toString();
   return barDiv;
 }
 
